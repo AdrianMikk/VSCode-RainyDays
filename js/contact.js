742a38cf6be21b4b32c68f919d7029d47fd3dab5
AdrianMikk <adrmik99436@stud.noroff.no>
2023-03-23 23:51:23 +0100

    updated form / contact

--- js/contact.js
+++ js/contact.js
@@ -1,75 +1,3 @@
-// const form = document.querySelector("#contactForm");
-// const contactButton = document.querySelector("#contactButton");
-// const errorMessages = document.querySelector("#message");
-
-// form.addEventListener("submit", function(event) {
-//   const firstName = document.querySelector("#firstName");
-//   const lastName = document.querySelector("#lastName");
-//   const subject = document.querySelector("#subject");
-//   const email = document.querySelector("#email");
-//   const message = document.querySelector("#message");
-
-//   let isValid = true;
-//   errorMessages.innerHTML = ""
-
-//   if (checkLength(firstName.value, 0) === false) {
-//     isValid = false;
-//     addErrorMessage("Please enter your first name.");
-//   }
- 
-//   if (checkLength(lastName.value, 3) === false) {
-//     isValid = false;
-//     addErrorMessage("Please enter your last name.");
-//   }
-
-//   if (validateEmail(email.value) === false) {
-//     isValid = false;
-//     addErrorMessage("Please enter a valid email address.");
-//   }
-
-//   if (checkLength(message.value, 6) === false) {
-//     isValid = false;
-//     addErrorMessage("Please enter at least 5 characters.");
-//   }
-
-//   if (checkLength(subject.value, 9) === false) {
-//     isValid = false;
-//     addErrorMessage("Please enter a subject.");
-//   }
-
-//   if (!isValid) {
-//     event.preventDefault();
-//   }
-
-//   if (isValid) {
-//     console.log("IT WORKS");
-//     event.preventDefault();
-//     // form.innerHTML = `<p class="succeedMessage">"Success! Your form has been submitted."</p>`
-//   }
-
-//   form.reset();
-// });
-
-// function addErrorMessage(message) {
-//   const errorMessage = document.createElement("div");
-//   errorMessage.classList.add("error-message");
-//   errorMessage.textContent = message;
-//   errorMessages.appendChild(errorMessage);
-// }
-
-//   function checkLength(value, len) {
-//     if (value.trim().length > len) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-
-//   function validateEmail(email) {
-//     const regEx = /\S+@\S+\.\S+/;
-//     const patternMatches = regEx.test(email);
-//     return patternMatches;
-//   }
 
 const form = document.querySelector("form");
 
@@ -147,3 +75,30 @@ function validateEmail(email) {
   const patternMatches = regEx.test(email);
   return patternMatches;
 }
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (
+    checkLength(firstNameInput.value, 1) &&
+    checkLength(lastNameInput.value, 3) &&
+    validateEmail(emailInput.value) && 
+    checkLength(messageInput.value, 9) && 
+    checkLength(subjectInput.value, 24)
+  ) {
+    form.innerHTML = `<div>
+                        <h3 style="text-align: center;">Thank you! Form has been submitted.
+                        </h3>
+                        <button id="successReturn"
+                        class=cta" style="padding: 5px;
+                        float: right;">Go back</button>
+                      </div>`;
+    let btn = document.querySelector("#successReturn");
+
+    btn.addEventListener("click", function () {
+      location.reload();
+    });
+  } else {
+    console.log("Complete form");
+  }
+});
